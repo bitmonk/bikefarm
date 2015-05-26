719823bc0360db121c47a1c0fbf024e966852ddd
Justin Alan Ryan <bitmonk@icloud.com>
2015-05-26 15:26:29 -0700

    add acceleration / deceleration tests.

--- src/main/java/com/bitmonk/bikefarm/BicycleTest.java
+++ src/main/java/com/bitmonk/bikefarm/BicycleTest.java
@@ -4,7 +4,7 @@ import org.junit.Test;
 
 public class BicycleTest {
     @Test
-    public void shiftsUp() {
+    public void test_shifts_up() {
         InternalHubBicycle upshiftbike = new InternalHubBicycle();
         int initialStatus = upshiftbike.gear;
         boolean result = upshiftbike.shiftUp();
@@ -14,13 +14,35 @@ public class BicycleTest {
     }
 
     @Test
-    public void shiftsDown() {
+    public void test_shifts_down() {
         InternalHubBicycle downshiftbike = new InternalHubBicycle();
         int initialStatus = downshiftbike.gear;
         boolean result = downshiftbike.shiftDown();
         int newStatus = downshiftbike.gear;
         assertTrue("failure - shiftDown returned non-true value.", result);
         assertEquals("failure - shiftDown did not result in expected gear change.", newStatus,initialStatus-1);
+    }
+
+    @Test
+    public void test_accelerates() {
+        InternalHubBicycle acceleratebike = new InternalHubBicycle();
+        int initialStatus = acceleratebike.speed;
+        boolean result = acceleratebike.speedUp(10);
+        int newStatus = acceleratebike.speed;
+        assertTrue("failure - speedUp returned non-true value.", result);
+        assertEquals("failure - speedUp did not result in expected speed increase.", newStatus,initialStatus+10);
+    }
+
+    @Test
+    public void test_decelerates() {
+        InternalHubBicycle deceleratebike = new InternalHubBicycle();
+        boolean accel_result = deceleratebike.speedUp(10);
+        int initialStatus = deceleratebike.speed;
+        boolean result = deceleratebike.applyBrakes(10);
+        int newStatus = deceleratebike.speed;
+        assertTrue("failure - speedUp returned non-true value.", accel_result);
+        assertTrue("failure - applyBrakes returned non-true value.", result);
+        assertEquals("failure - speedUp did not result in expected speed decrease.", newStatus,initialStatus+10);
     }
 
 }
